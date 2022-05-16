643fefeb7000ef15875d75b17c95c73f7de053a6
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-16 13:21:34 +0200

    Initial Commit

---
+++ liststarter/liststarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  liststarterApp.swift
+//  liststarter
+//
+//  Created by Cloud Secrets on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct liststarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
